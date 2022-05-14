2f42bfde6440198117adc81f13e694be23339feb
Samuel Bartolomedi <samuelbartolomedi1994@gmail.com>
2022-05-14 02:39:19 -0300

    first request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,11 +1,20 @@
+import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import { BASE_URL } from "utils/requests";
 
 
 function Listing() {
+
+    //form incorreta
+    axios.get(`${BASE_URL}/movies?size=12&page=0`)
+        .then(response => {
+            console.log(response.data);
+        });
+
     return (
         <>
-        <Pagination />
+            <Pagination />
             <div className="container">
                 <div className="row">
                     <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
